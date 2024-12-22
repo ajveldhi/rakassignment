d07936346faba72897eb2efbecdde3c1fd35278f
ajveldhi <ajveldhi@gmail.com>
2024-12-22 10:33:27 +0000

    deleted aws cli

--- eks_manual_creation/1-provider.tf
+++ eks_manual_creation/1-provider.tf
@@ -1,19 +1,24 @@
 
 provider "aws" {
   region = local.region
-
 }
 
 terraform {
   required_version = ">= 1.0"
 
   required_providers {
-
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.9 "
+      version = "~> 5.9"
     }
+  }
 
+  backend "s3" {
+    bucket         = "11my-terraform-state-bucket"
+    key            = "path/to/my/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 
 }
